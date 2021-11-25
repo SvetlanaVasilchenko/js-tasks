99e68fb4f2b46cd21df0eb09374c806ce54ed035
Vasilchenko Svetlana <s.o.vasilchenko@yandex.ru>
2021-11-25 17:52:24 +0300

    add implementation to the strings-2 task

--- tasks/strings/2/strings-2.js
+++ tasks/strings/2/strings-2.js
@@ -2,5 +2,14 @@
  * Function should return taken string with first char in upper case
  */
 export default function upperCaseFirst(str) {
-    return str;
+    if (str === "") {
+        return str;
+    } 
+    else {
+        let firstSimbol = str[0];
+        let newStr = firstSimbol.toUpperCase() + str.slice(1);
+        return newStr;
+
+    }
+    
 }
