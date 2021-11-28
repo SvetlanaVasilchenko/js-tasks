93be4bfc8f942d6e4c684150aeebb1b722b5324b
Vasilchenko Svetlana <s.o.vasilchenko@yandex.ru>
2021-11-28 18:53:13 +0300

    get implementation arrays-1 task

--- tasks/arrays/1/arrays-1.js
+++ tasks/arrays/1/arrays-1.js
@@ -3,6 +3,25 @@
  * and should turn all negative numbers into positive
  * and return this array
  */
-export default function (array) {
-    return array;
+export default function f (array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            newArray.push(array[i] * (-1));
+        }
+        else {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 }
+
+/**
+ * найти все отриц. числа и превратить в положительные
+ *
+ *
+ *
+ **/
+
+
+
